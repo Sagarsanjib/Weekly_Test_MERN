9306a9acf718a80737c0ed502ce91905b103aece
Sagarsanjib <96881153+Sagarsanjib@users.noreply.github.com>
2022-05-10 09:19:45 +0530

    Add files via upload

---
+++ Weekly Test - 2022/6. March 06 Test/index.js
@@ -0,0 +1,36 @@
+function detail1()
+{
+var product=document.getElementById("img1");
+var pd=document.getElementById("product1");
+pd.style.display=product.click?"block":"none";
+}
+function detail2()
+{
+var product=document.getElementById("img2");
+var pd=document.getElementById("product2");
+pd.style.display=product.click?"block":"none";
+}
+function detail3()
+{
+var product=document.getElementById("img3");
+var pd=document.getElementById("product3");
+pd.style.display=product.click?"block":"none";
+}
+function detail4()
+{
+var product=document.getElementById("img4");
+var pd=document.getElementById("product4");
+pd.style.display=product.click?"block":"none";
+}
+function detail5()
+{
+var product=document.getElementById("img5");
+var pd=document.getElementById("product5");
+pd.style.display=product.click?"block":"none";
+}
+function detail6()
+{
+var product=document.getElementById("img6");
+var pd=document.getElementById("product6");
+pd.style.display=product.click?"block":"none";
+}
